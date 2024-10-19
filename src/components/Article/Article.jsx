a0056dcb99e98209cae959516d3207e9f60a259f
Ricardo Martins <ricardomartins.com>
2024-10-19 18:19:30 -0300

    Corrigindo problemas de estilo nos arquivos CSS

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,3 +3,23 @@
 // Create an Article function returning the HTML of article block
 
 // Add a default export statement for Article component to use it in the other files
+// Article.jsx
+import React from 'react';
+import './Article.css'; // Importa o arquivo CSS contendo os estilos do artigo
+
+// Cria o componente Article que retorna o HTML do bloco do artigo
+const Article = () => {
+  return (
+    <div className="article">
+      <h1>Headline</h1>
+      <p className="article__paragraph">Paragraph 1</p>
+      <p className="article__paragraph">Paragraph 2</p>
+      <p className="article__paragraph">Paragraph 3</p>
+      <p className="article__paragraph">Paragraph 4</p>
+      <p className="article__paragraph">Paragraph 5</p>
+    </div>
+  );
+};
+
+// Adiciona uma declaração de exportação padrão para o componente Article
+export default Article;
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,3 +3,16 @@
 // Create a Welcome function returning the HTML of welcome block
 
 // Add a default export statement for Welcome component to use it in the other files
+// Import a css file containing welcome styles
+import './Welcome.css';
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <h1 className="welcome__text">Sticky Header!</h1>
+    </div>
+  );
+};
+
+// Add a default export statement for Welcome component to use it in the other files
+export default Welcome;
